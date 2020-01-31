b39fc3ea8b86c69396adaf92a5dc244bbc397edf
Gean Brandao <gean.kar@brave.ag>
2020-01-31 14:31:45 -0200

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Para inicializar o projeto
+$ yarn init -y
+
+Para testar e rodar o arquivo que cria o servidor
+$ node src/server.js
+
+$ yarn add express
+$ node ./src/server.js
---
+++ src/server.js
@@ -0,0 +1,6 @@
+const app = require('./app');
+
+app.listen(3333);
+
+// fazendo assim se separa a parte onde se cria as rotas da parte onde se 
+// cria o servidor que no caso é esse arquivo
