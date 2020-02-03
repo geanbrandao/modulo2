74060f276670b633a0c50fa036a82457dd47524e
Gean Brandao <gean.kar@brave.ag>
2020-02-02 22:17:42 -0200

    add post user

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,26 @@
+import User from '../models/User';
+
+class UserController {
+  // recebe os mesmos parametros do node
+  async store(req, res) {
+    const userExist = await User.findOne({ where: { email: req.body.email } });
+
+    if (userExist) {
+      // passa o body e o sequelize se acha pelo nome dos campos
+      return res.status(400).json({ message: 'Email já cadastrado.' });
+    }
+
+    const { id, name, email, provider } = await User.create(req.body);
+
+    return res.json({
+      usuario: {
+        id,
+        name,
+        email,
+        provider,
+      },
+    });
+  }
+}
+
+export default new UserController();
--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,9 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-// funcao precisa ser asyncrona pq pode demorar pra executar
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Gean Carlos Brandao',
-    email: 'gean.kar@brave.ag',
-    password_hash: '1234567',
-  });
-
-  res.json({ usuario: user });
-});
+routes.post('/user', UserController.store);
 
 export default routes;
